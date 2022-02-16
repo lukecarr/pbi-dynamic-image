c37a2bc162d51002cefe88c090ad05d292fb327f
Luke Carr <24438483+lukecarr@users.noreply.github.com>
2022-02-16 15:23:10 +0000

    ⬆️Bumped dependencies

--- src/visual.ts
+++ src/visual.ts
@@ -1,20 +1,19 @@
-"use strict";
 import powerbiVisualsApi from "powerbi-visuals-api";
-
 import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbiVisualsApi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbiVisualsApi.extensibility.visual.IVisual;
 
-// Import React dependencies and the added component
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import component, { State } from "./component";
+import component from "./component";
 
 import "./../style/visual.less";
 
 // noinspection JSUnusedGlobalSymbols
 export class Visual implements IVisual {
     private readonly target: HTMLElement;
+    private src = "";
 
     constructor(options: VisualConstructorOptions) {
         this.target = options.element;
@@ -23,15 +22,14 @@ export class Visual implements IVisual {
     }
 
     public update(options: VisualUpdateOptions) {
-        this.render({
-            src: options.dataViews && options.dataViews[0] && options.dataViews[0].single.value.toString() || "",
-        });
+        const src = options.dataViews[0]?.single?.value.toString() || "";
+        if (src !== this.src) {
+            this.src = src;
+            this.render();
+        }
     }
 
-    private render(state?: State) {
-        ReactDOM.render(React.createElement(component, {
-            src: "",
-            ...state,
-        }), this.target);
+    private render() {
+        ReactDOM.render(React.createElement(component, { src: this.src }), this.target);
     }
 }
